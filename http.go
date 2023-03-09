8e2abd72e3f4fbd91d1fb9a2230dc05165d6abba
maseer666 <ma@office>
2023-03-09 17:50:34 +0800

    dev

--- http.go
+++ http.go
@@ -6,34 +6,114 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"golang.org/x/net/proxy"
 )
+
+type NetState int
+
+const (
+	NetStateUnknow NetState = 0
+	NetStateOK     NetState = 1
+	NetStateFaild  NetState = 2
+)
+
+type ConfigPing struct {
+	Timeout time.Duration
+	URLPing string
+}
 
 type HttpClient struct {
 	Headers     map[string]string
 	Cookies     map[string]string
-	ProxySocks5 string //like "127.0.0.1:7891"
-	client      *http.Client
+	ProxySocks5 string //like "127.0.0.1:7891",or set env "socks5_proxy"
+	RetryTimes  int
+	*ConfigPing
+
+	client       *http.Client
+	netState     NetState
+	lockCheckNet sync.Mutex
 }
 
 func DefaultClient() *HttpClient {
-	return &HttpClient{
-		client:  &http.Client{},
-		Headers: map[string]string{},
-		Cookies: map[string]string{},
+	client := &HttpClient{
+		client: &http.Client{
+			Timeout: time.Second * 2,
+		},
+		Headers:      map[string]string{},
+		Cookies:      map[string]string{},
+		lockCheckNet: sync.Mutex{},
 	}
+	if v := os.Getenv("socks_proxy"); v != "" {
+		client.SetSocks5Proxy(v)
+	}
+	return client
 }
 
-func (client *HttpClient) do(req *http.Request) (*http.Response, error) {
-	for k, v := range client.Headers {
+func (c *HttpClient) WaitPing() error {
+	c.lockCheckNet.Lock()
+	defer c.lockCheckNet.Unlock()
+	if c.netState == NetStateFaild {
+		return fmt.Errorf("network error[0]")
+	} else if c.netState == NetStateOK {
+		return nil
+	} else if c.netState == NetStateUnknow && c.ConfigPing == nil {
+		return nil
+	}
+	req, err := http.NewRequest(http.MethodGet, c.ConfigPing.URLPing, nil)
+	if err != nil {
+		return nil
+	}
+	timeStart := time.Now()
+	for {
+		resp, err := c.client.Do(req)
+		if err != nil {
+			if time.Now().After(timeStart.Add(c.ConfigPing.Timeout)) {
+				c.netState = NetStateFaild
+				return fmt.Errorf("network error[1]")
+			}
+			continue
+		}
+		if resp.StatusCode == http.StatusOK {
+			c.netState = NetStateOK
+			return nil
+		}
+		<-time.After(time.Second * 10)
+	}
+}
+func (c *HttpClient) SetSocks5Proxy(socks5 string) {
+	dialSocksProxy, err := proxy.SOCKS5("tcp", socks5, nil, proxy.Direct)
+	if err != nil {
+		fmt.Println("Error connecting to proxy:", err)
+	}
+	tr := &http.Transport{Dial: dialSocksProxy.Dial}
+	c.client.Transport = tr
+}
+
+func (c *HttpClient) do(req *http.Request, retryTimes int) (*http.Response, error) {
+	if err := c.WaitPing(); err != nil {
+		return nil, err
+	}
+	for k, v := range c.Headers {
 		req.Header.Set(k, v)
 	}
-	for k, v := range client.Cookies {
+	for k, v := range c.Cookies {
 		req.AddCookie(&http.Cookie{
 			Name:  k,
 			Value: v,
 		})
 	}
-	return client.client.Do(req)
+	resp, err := c.client.Do(req)
+	if err != nil {
+		if retryTimes > 0 {
+			return c.do(req, retryTimes-1)
+		}
+		return nil, err
+	}
+	return resp, err
 }
 
 func (client *HttpClient) Http(u string, method string, jsonObject any) (*http.Response, error) {
@@ -50,7 +130,7 @@ func (client *HttpClient) Http(u string, method string, jsonObject any) (*http.R
 	if err != nil {
 		return nil, err
 	}
-	resp, err := client.do(req)
+	resp, err := client.do(req, client.RetryTimes)
 	if err != nil {
 		return nil, err
 	}
